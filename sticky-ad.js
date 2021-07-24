4d090d05402bc366c57a800bdadf3fd697eb5cfa
amaterasublog <57845299+amaterasublog@users.noreply.github.com>
2021-07-24 13:41:58 +0700

    Add files via upload

---
+++ sticky-ad.js
@@ -0,0 +1,6 @@
+var uri = window.location.toString();if (uri.indexOf(&quot;?m=1&quot;,&quot;?m=1&quot;) &gt; 0) {var clean_uri = uri.substring(0, uri.indexOf(&quot;?m=1&quot;));window.history.replaceState({}, document.title, clean_uri);} </script>
+    <b:if cond='data:blog.pageType not in {&quot;static_page&quot;,&quot;error_page&quot;} and data:blog.isMobileRequest == &quot;false&quot; and not data:blog.searchQuery'>
+<div class='sticky-ad' id='sticky-ad'>
+<ins class='adsbygoogle' data-ad-client='ca-pub-2328569725402464' data-ad-slot='1946233145' style='display:inline-block;width:728px;height:90px'/>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
