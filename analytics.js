2f66e5288e375f9a51795dcdfaeb1892d06d300e
amaterasublog <57845299+amaterasublog@users.noreply.github.com>
2021-07-24 13:20:07 +0700

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,8 +1,4 @@
-    <script>
-//<![CDATA[
 function downloadJSAtOnload(){var d=document.createElement("script");d.src="https://www.google-analytics.com/analytics.js",document.body.appendChild(d)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
 window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
 ga('create', 'UA-137346170-1', 'auto');
 ga('send', 'pageview');
-//]]>
-</script>
