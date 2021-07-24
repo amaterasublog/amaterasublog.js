686a9ecc4409521ed3caade85ed9332ffb1a4f9f
amaterasublog <57845299+amaterasublog@users.noreply.github.com>
2021-07-24 13:45:39 +0700

    Add files via upload

---
+++ m-1.js
@@ -0,0 +1,1 @@
+var uri = window.location.toString();if (uri.indexOf(&quot;?m=1&quot;,&quot;?m=1&quot;) &gt; 0) {var clean_uri = uri.substring(0, uri.indexOf(&quot;?m=1&quot;));window.history.replaceState({}, document.title, clean_uri);}
